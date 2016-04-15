12d0368d6285c363400d8407d899556e4aa20156
pazdyga@pythian.com <pazdyga@pythian.com>
2016-04-15 17:08:07 +0200

    Added some comments and description to the code

--- production/manifests/site.pp
+++ production/manifests/site.pp
@@ -1,9 +1,19 @@
+### This is a main configuration file. We can route the configuration to the hosts here.
+#
+
+### Default host definition
+#
 node default inherits basenode{
 }
 
+### Basenode definition
+#
 node basenode {
   include base
 }
+
+### This a name based assignement. It could be done otherwise or skipped and hardcoded. Leaving that as is for demo purposes.
+#
 
 node /^app(.*)$/ inherits basenode {
   include web
