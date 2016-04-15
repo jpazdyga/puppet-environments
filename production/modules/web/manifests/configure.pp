8fc5b44fda77020e358da274a2d80a8b271006cd
pazdyga@pythian.com <pazdyga@pythian.com>
2016-04-15 10:32:58 +0200

    Testing augeas httpd.conf

--- production/modules/web/manifests/configure.pp
+++ production/modules/web/manifests/configure.pp
@@ -3,7 +3,8 @@ class web::configure ($apache_listen_port=hiera('apache_listen_port')) {
   augeas { "/etc/httpd/conf/httpd.conf":
     context => "/files/etc/httpd/conf/httpd.conf",
     changes => [
-      "set 'Listen' '$apache_listen_port'",
+      "set /files/etc/httpd/conf/httpd.conf/directive 'Listen'",
+      "set /files/etc/httpd/conf/httpd.conf/*[self::directive="Listen"]/arg '$apache_listen_port'",
     ],
   }
 
