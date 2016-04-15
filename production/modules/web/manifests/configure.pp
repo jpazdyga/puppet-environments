39a321f47755c1de6a48218752143074e2f79006
pazdyga@pythian.com <pazdyga@pythian.com>
2016-04-15 10:27:00 +0200

    Added extended web module

--- production/modules/web/manifests/configure.pp
+++ production/modules/web/manifests/configure.pp
@@ -1,7 +1,10 @@
 class web::configure ($apache_listen_port=hiera('apache_listen_port')) {
 
-  augeas { "":
-
+  augeas { "/etc/httpd/conf/httpd.conf":
+    context => "/files/etc/httpd/conf/httpd.conf",
+    changes => [
+      "set 'Listen' '$apache_listen_port'",
+    ],
   }
 
 }
