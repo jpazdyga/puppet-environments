03bf39f672a9f46c79beb0b3bffd1b55f1eb10e4
pazdyga@pythian.com <pazdyga@pythian.com>
2016-04-15 12:14:34 +0200

    Added selinux puppet module

--- production/modules/base/manifests/install.pp
+++ production/modules/base/manifests/install.pp
@@ -1,40 +1,9 @@
 class base::install ($min_memory_size=hiera('min_memory_size'), $max_memory_size=hiera('max_memory_size')) {
 
-  file { "/etc/dgm.properties":
-    ensure => present,
-  }
-
-  augeas { "/etc/dgm.properties":
-    context => "/files/etc/dgm.properties",
-    incl => "/etc/dgm.properties",
-    lens => "Shellvars.lns",
-    changes => [
-      "set 'min_memory_size' '$min_memory_size'",
-      "set 'max_memory_size' '$max_memory_size'",
-    ],
-  require => File["/etc/dgm.properties"],
-  }
-
-  file { "/etc/facter/":
-    ensure => directory,
-  }
-
-  file { "/etc/facter/facts.d/":
-    ensure => directory,
-    require => File[ "/etc/facter/" ],
-  }
-
-  file { "/etc/facter/facts.d/environment.sh":
-    source => "puppet:///modules/base/environment.sh",
-    mode => 755,
-  }
-
-  file { "/etc/facter/facts.d/baseline.txt":
-    source => "puppet:///modules/base/baseline.txt",
-  }
-
-  file { "/etc/facter/facts.d/role.txt":
-    source => "puppet:///modules/base/role.txt",
+  exec { "install-selinux":
+    path => "/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/vagrant/.local/bin:/home/vagrant/bin",
+    command => '/usr/bin/puppet module install spiette/selinux',
+    creates => "/etc/puppet/modules/selinux",
   }
 
 }
