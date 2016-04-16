f6ff74d6a95aaf0166408196961d75da710f1457
pazdyga@pythian.com <pazdyga@pythian.com>
2016-04-16 06:32:08 +0200

    Testing the environment fact not to be overwritten

--- production/modules/base/manifests/configure.pp
+++ production/modules/base/manifests/configure.pp
@@ -33,7 +33,7 @@ class base::configure ($min_memory_size=hiera('min_memory_size'), $max_memory_si
 
   file { "/etc/facter/facts.d/environment.txt":
     source => "puppet:///modules/base/environment.txt",
-    onlyif => ""/usr/bin/test -s /etc/facter/facts.d/environment.txt",
+    onlyif => "/usr/bin/test -s /etc/facter/facts.d/environment.txt",
   }
 
   file { "/etc/facter/facts.d/baseline.txt":
