1c7fcf3101bf3e63da08f77bbefbf7e35a1a9728
pazdyga@pythian.com <pazdyga@pythian.com>
2016-04-18 06:29:50 +0200

    Custom function example

--- production/modules/base/manifests/install.pp
+++ production/modules/base/manifests/install.pp
@@ -1,3 +1,7 @@
 class base::install {
 
+  include customfunct
+
+  customfunct('/tmp/customfunct-output', "This is a call from base module!"')
+
 }
---
+++ production/modules/customfunct/lib/puppet/parser/functions/customfunct.rb
@@ -0,0 +1,9 @@
+module Puppet::Parser::Functions
+  newfunction(:customfunct) do |args|
+    filename = args[0]
+    test = args[1]
+    userid = `id`
+    str = "This is a test: " + test + ". Here's the id info: " + userid
+    File.open(filename, 'a') {|fd| fd.puts str }
+  end
+end
